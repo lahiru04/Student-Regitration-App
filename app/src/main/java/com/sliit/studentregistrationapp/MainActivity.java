f96fddabe9d19ddcab4e2df239d21e5480b51106
Lahiru Prasanna <prasannalahiru@gmail.com>
2021-09-05 13:01:04 +0530

    text views , images , icons are added

--- app/src/main/java/com/sliit/studentregistrationapp/MainActivity.java
+++ app/src/main/java/com/sliit/studentregistrationapp/MainActivity.java
@@ -3,12 +3,40 @@ package com.sliit.studentregistrationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
+    private EditText editTextStudentName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = findViewById(R.id.button);
+        editTextStudentName = findViewById(R.id.editTextStudentName);
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String val = editTextStudentName.getText().toString();
+
+            }
+        });
+
+
     }
 }
